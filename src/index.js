a27797729afe8f7f111eac656f107e66177cf6bb
Đông Trần Ngọc <57401513+ngocdong1999@users.noreply.github.com>
2021-06-02 16:33:40 +0700

    update 2

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const path = require('path');
 const morgan = require('morgan');
+const handlebars = require('express-handlebars');
+
 const app = express();
 const port = 3000;
 
-app.use(morgan('combined'))
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/',(req,res) => res.send('Hello World!'));
+app.engine('hbs', handlebars({
+    extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname,'resources/views'));
+app.use(morgan('combined'));
+
+app.get('/',(req,res) => res.render('home'));
+app.get('/news',(req,res) => res.render('news'));
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
