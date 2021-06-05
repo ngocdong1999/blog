688b8b8e9c07e4f18b744b42c7e66ec97f428cb7
Đông Trần Ngọc <57401513+ngocdong1999@users.noreply.github.com>
2021-06-05 23:47:55 +0700

    refactor code

--- src/index.js
+++ src/index.js
@@ -6,7 +6,14 @@ const handlebars = require('express-handlebars');
 const app = express();
 const port = 3000;
 
+const route = require('./routes')
+
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.urlencoded({
+    extended: true,
+}));
+app.use(express.json());
 
 app.engine('hbs', handlebars({
     extname: '.hbs'
@@ -15,7 +22,8 @@ app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname,'resources/views'));
 app.use(morgan('combined'));
 
-app.get('/',(req,res) => res.render('home'));
-app.get('/news',(req,res) => res.render('news'));
+
+route(app);
+
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
