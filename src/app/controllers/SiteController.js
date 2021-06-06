6e0be2584864c3fecca8e7bc5c7c15f796755115
Đông Trần Ngọc <57401513+ngocdong1999@users.noreply.github.com>
2021-06-06 23:02:26 +0700

    create model connect mongodb

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,8 +1,19 @@
+const Course = require('../models/Course');
+const { mutipleMongooseToObject } = require('../../util/mongoose')
 
 class SiteController {
 
-    index(req,res){
-        res.render('home');
+    index(req, res, next){
+
+        Course.find({})
+            .then(courses => {
+                res.render('home',{ 
+                    courses: mutipleMongooseToObject(courses)
+                });
+            })
+            .catch(next);
+
+        // res.render('home');
     }
     search(req,res){
         res.render('search');
--- src/index.js
+++ src/index.js
@@ -6,7 +6,11 @@ const handlebars = require('express-handlebars');
 const app = express();
 const port = 3000;
 
-const route = require('./routes')
+const route = require('./routes');
+const db = require('./config/db');
+
+// Connect to db
+db.connect();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -19,11 +23,11 @@ app.engine('hbs', handlebars({
     extname: '.hbs'
 }));
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname,'resources/views'));
+app.set('views', path.join(__dirname,'resources','views'));
 app.use(morgan('combined'));
 
 
 route(app);
 
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.listen(port, () => console.log(`App listening at http://localhost:${port}`))
